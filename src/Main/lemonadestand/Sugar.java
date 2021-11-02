a8d002f68110d443b249d810b2da74cc19b92ef5
Dylan harris <dylanharris23@gmail.com>
2021-11-02 11:08:33 -0400

    Classes done

---
+++ src/Main/lemonadestand/Sugar.java
@@ -0,0 +1,15 @@
+package Main.lemonadestand;
+
+public class Sugar {
+
+	int amount;
+
+	String unit;
+
+	public Sugar(int amount, String unit) {
+		super();
+		this.amount = amount;
+		this.unit = unit;
+	}
+
+}
---
+++ src/Main/lemonadestand/Water.java
@@ -0,0 +1,14 @@
+package Main.lemonadestand;
+
+public class Water {
+
+	int amount;
+
+	public Water(int amount, String unit) {
+		super();
+		this.amount = amount;
+		this.unit = unit;
+	}
+
+	String unit;
+}
