3233a3c265c6098b81cad07d0206aac261286454
Dylan harris <dylanharris23@gmail.com>
2021-11-02 12:04:29 -0400

    Finished objects

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,35 +2,28 @@ package Main;
 
 //import java.util.Iterator;
 
+import Main.lemonadestand.Water;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(add(1, 5));
-		System.out.println(concat("Hello", "World"));
-		System.out.println(divide(6, 0));
+		
+		Water waterOne = new Water(5, "cups");
+		
+		System.out.println(waterOne.drink(3));
+		
+		System.out.println(waterOne.amount + " " + waterOne.unit);
 	}
-
+	
 	public static int add(int a, int b) {
 		return a + b;
 	}
-
+	
 	public static String concat(String a, String b) {
 		return a + b;
 	}
-
-	/**
-	 * Divides numbers and returns the result
-	 * 
-	 * @param a - the number to divide
-	 * @param b - the number to divide by
-	 * @return - the result of dividing a by b
-	 * @throws IllegalArgumentException - if b equals 0
-	 */
-
-	public static int divide(int a, int b) throws IllegalArgumentException {
-		if (b == 0) {
-			throw new IllegalArgumentException();
-		}
+	
+	public static int divide(int a, int b) {
 		return a / b;
 	}
 }
--- src/Main/lemonadestand/Water.java
+++ src/Main/lemonadestand/Water.java
@@ -2,13 +2,21 @@ package Main.lemonadestand;
 
 public class Water {
 
-	int amount;
+	public int amount;
+	public String unit;
 
 	public Water(int amount, String unit) {
 		super();
 		this.amount = amount;
 		this.unit = unit;
 	}
+	
+	public void drink(int amount) {
+		if (this.amount < amount) {
+			throw new IllegalArgumentException("You cannot drink more water than is available. Amount of water available: " + this.amount);
+		} else {
+			this.amount -= amount;
+		}
+	}
 
-	String unit;
 }
